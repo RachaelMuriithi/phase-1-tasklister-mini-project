7ac7f76457d27f6faf5fb9f0cef883e368ede180
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-08-25 00:47:34 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submit = document.querySelector ("#create_task_form")
+  submit.addEventListener ('submit', (e)=> {
+    e.preventDefault()
+    const inputValue = (document.getElementById ('new_task_description').value)
+    // console.log (e.target.new_task_description.value)
+    buildToDo (inputValue)
+    submit.reset ()
+  })
 });
+
+function buildToDo (todo) {
+  let li = document.createElement ('li')
+  let btn = document. createElement ('button')
+  btn. textContent = ' x'
+  li.textContent = todo
+  li.appendChild (btn)
+  ul = document.querySelector ('#tasks')
+  ul.appendChild (li);
+  btn.addEventListener ('click', () => {
+    li.remove()
+  })
+}
